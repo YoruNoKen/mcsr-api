bfd2959a9ea6759adcafbbd0214b5c58f28054ec
YoruNoKen <m.f.fetvaci@hotmail.com>
2023-04-10 19:06:34 +0300

    merged some of the endpoints

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 class ranked_api {
 	static baseURL = "https://mcsrranked.com/api";
 
-	async getUserStats(username) {
-		const url = `${ranked_api.baseURL}/users/${username}`;
+	async getUserStats(username, opponent) {
+		let url = `${ranked_api.baseURL}/users/${username}`;
+		if (opponent !== undefined) {
+			url = `${ranked_api.baseURL}/users/${username1}/versus/${username2}`;
+		}
 
 		let response;
 		try {
@@ -14,23 +17,6 @@ class ranked_api {
 		if (response.status == "error") {
 			throw new Error("Invalid Username");
 		}
-		return response.data;
-	}
-
-	async getUserVersus(username1, username2) {
-		const url = `${ranked_api.baseURL}/users/${username1}/versus/${username2}`;
-
-		let response;
-		try {
-			response = await fetch(url).then((res) => res.json());
-		} catch (err) {
-			return err;
-		}
-
-		if (response.status == "error") {
-			throw new Error("Invalid Username(s)");
-		}
-
 		return response.data;
 	}
 
@@ -47,24 +33,16 @@ class ranked_api {
 		return response.data;
 	}
 
-	async getRecentMatch(username, { match_type, page, count }) {
+	async getRecentMatch(username, { opponent, match_type, page, count }) {
 		let _type = match_type;
 		let _page = page;
 		let _count = count;
+		const filter = addFilters(_type, _page, _count);
 
-		let _filter = "";
-		if (_type !== undefined) {
-			_filter += `&filter=${_type}`;
+		let url = `${ranked_api.baseURL}/users/${username}/matches${filter}`;
+		if (opponent !== undefined) {
+			url = `${ranked_api.baseURL}/users/${username}/versus/${opponent}/matches${filter}`;
 		}
-		if (_page !== undefined) {
-			_filter += `&page=${_page}`;
-		}
-		if (_count !== undefined) {
-			_filter += `&count=${_count}`;
-		}
-		_filter = _filter.replace("/^&/", "?");
-
-		const url = `${ranked_api.baseURL}/users/${username}/matches${_filter}`;
 
 		let response;
 		try {
@@ -83,42 +61,21 @@ class ranked_api {
 		return response.data;
 	}
 
-	async getVersusMatch(username1, username2, { match_type, page, count }) {
-		let _type = match_type;
-		let _page = page;
-		let _count = count;
+}
 
-		let _filter = "";
-		if (_type !== undefined) {
-			_filter += `&filter=${_type}`;
-		}
-		if (_page !== undefined) {
-			_filter += `&page=${_page}`;
-		}
-		if (_count !== undefined) {
-			_filter += `&count=${_count}`;
-		}
-		_filter = _filter.replace("/^&/", "?");
-
-		const url = `${ranked_api.baseURL}/users/${username1}/versus/${username2}/matches${_filter}`;
-
-		let response;
-		try {
-			response = await fetch(url).then((res) => res.json());
-		} catch (err) {
-			return err;
-		}
-
-		if (response.status == "error") {
-			if (response.data == null) {
-				throw new Error("Invalid Username(s)");
-			} else {
-				throw new Error(response.data);
-			}
-		}
-
-		return response.data;
+function addFilters(match_type, page, count) {
+	let _filter = "";
+	if (match_type !== undefined) {
+		_filter += `&filter=${match_type}`;
 	}
+	if (page !== undefined) {
+		_filter += `&page=${page}`;
+	}
+	if (count !== undefined) {
+		_filter += `&count=${count}`;
+	}
+	_filter = _filter.replace("/^&/", "?");
+	return _filter;
 }
 
 module.exports.ranked_api = ranked_api;
